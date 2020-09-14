02a50e8e3f8b50e710148edd6ad4ab46e25862c9
travis skyles <travis.skyles@gmail.com>
2020-09-14 01:11:15 -0700

    menu and home page styling complete

--- js/header.js
+++ js/header.js
@@ -2,4 +2,9 @@ $(function () {
 	$('#attr_info').hover(function () {
 		$('#attr_box').toggleClass('hidden');
 	});
+
+	$('.icon-wrapper').hover(function () {
+		// $(this).children(':first').toggleClass('hidden');
+		$(this).children(':last').toggleClass('hidden');
+	});
 });
